45e3a7c2d8af96bbc3d73d23aa217b2b61857bf1
Ola <129540485+Maladelfineka@users.noreply.github.com>
2024-04-09 17:19:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,124 @@
+let root = document.querySelector('.root');
 
-var text = document.querySelector(".text");
+let id = '';
+
+let notatki = localStorage.getItem("notatki");
+
+let notatkiArray = localStorage.getItem("notatki").split(' ');
+
+localStorage.removeItem(id);
 
 
+let add = document.querySelector('.add');
 
-var Delete = document.querySelector(".delete");
+let deleteButton = document.querySelector('.delete');
 
-var save = document.querySelector(".save");
+let textarea = document.querySelector('#text');
 
+console.log(textarea);
 
+function notatkiWidok() {
 
-save.onclick = function(){
-    localStorage.setItem("notatka", text.value);
+    if (notatki) {
+
+        for (let i of notatkiArray) {
+
+            if(i){
+
+                root.innerHTML += `
+
+                <div class="notatki1">
+
+                    ${localStorage.getItem(i)}
+
+                    <button onclick="usun(${i})" >-</button>
+
+                </div>
+
+                `
+
+            }
+
+            
+
+        }
+
+    }
+
 }
 
-if(localStorage.getItem("notatka")) {
-    text.innerHTML = localStorage.getItem("notatka");
+function usun(id) {
+
+    console.log(id);
+
+    localStorage.removeItem(id);
+
+    console.log(id);
+
+    let noweNotatki = notatki.replace(" " + id, '');
+
+    console.log(noweNotatki);
+
+    localStorage.setItem('notatki', noweNotatki);
+
+    notatki = localStorage.getItem("notatki");
+
+    notatkiArray = localStorage.getItem("notatki").split(' ');
+
+    console.log(notatkiArray);
+
+    root.innerHTML = '';
+
+    notatkiWidok();
+
+    
+
 }
 
-Delete.onclick = function(){
-    localStorage.setItem("notatka", "");
-    text.textContent = "";
+add.onclick = () => {
+
+    id = Date.now();
+
+    console.log(textarea.value);
+
+    localStorage.setItem(id, textarea.value);
+
+    localStorage.setItem("notatki", notatki + " " + id);
+
+    notatki = localStorage.getItem("notatki");
+
+    notatkiArray = localStorage.getItem("notatki").split(' ')
+
+    console.log(notatkiArray);
+
+    root.innerHTML = '';
+
+    notatkiWidok();
+
 }
+
+deleteButton.onclick = () => {
+
+
+    localStorage.removeItem(id);
+
+    console.log(id);
+
+    let noweNotatki = notatki.replace(" " + id, '');
+
+    console.log(noweNotatki);
+
+    localStorage.setItem('notatki', noweNotatki);
+
+    notatkiArray = localStorage.getItem("notatki").split(' ');
+
+    console.log(notatkiArray);
+
+}
+
+
+
+
+notatkiWidok();
+
+///6 min do odrobienia
